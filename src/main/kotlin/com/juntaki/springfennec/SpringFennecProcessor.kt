e98a03b9099663844eba583b770c0371365a2bcf
juntaki <me@juntaki.com>
2017-09-10 23:47:36 +0900

    experiment

--- src/main/kotlin/com/juntaki/springfennec/SpringFennecProcessor.kt
+++ src/main/kotlin/com/juntaki/springfennec/SpringFennecProcessor.kt
@@ -1,6 +1,7 @@
 package com.juntaki.springfennec
 
 import com.google.auto.service.AutoService
+import io.swagger.annotations.Api
 import javax.annotation.processing.*
 import javax.lang.model.SourceVersion
 import javax.lang.model.element.TypeElement
@@ -8,10 +9,19 @@ import javax.tools.Diagnostic
 
 @AutoService(Processor::class)
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
-@SupportedAnnotationTypes("*")
+@SupportedAnnotationTypes("io.swagger.annotations.*", "org.springframework.web.bind.annotation.*")
 class SpringfennecProcessor : AbstractProcessor() {
-  override fun process(annotations: MutableSet<out TypeElement>, roundEnv: RoundEnvironment): Boolean {
-    processingEnv.messager.printMessage(Diagnostic.Kind.WARNING, "AAAAAAAAAAAAAAAAAAA")
-    return true
-  }
+    override fun process(annotations: MutableSet<out TypeElement>, roundEnv: RoundEnvironment): Boolean {
+        processingEnv.messager.printMessage(Diagnostic.Kind.WARNING, "Test")
+
+        roundEnv.getElementsAnnotatedWith(Api::class.java).forEach {
+            processingEnv.messager.printMessage(Diagnostic.Kind.WARNING, "ELEMENT: " + it.toString())
+            it.enclosedElements.forEach {
+                processingEnv.messager.printMessage(Diagnostic.Kind.WARNING, " enclosed: " + it.toString())
+                processingEnv.messager.printMessage(Diagnostic.Kind.WARNING, " enclosed: " +
+                        processingEnv.elementUtils.getAllAnnotationMirrors(it))
+            }
+        }
+        return true
+    }
 }
